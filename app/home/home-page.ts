b70027f8a7bf4e82ce310a860ae2d4132bfc114a
Akash Singhal <akashsinghal@AkashSinghal.local>
2019-03-13 15:53:59 +0200

    adding tabs and layout for home page

--- app/home/home-page.ts
+++ app/home/home-page.ts
@@ -1,7 +1,11 @@
+
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
-import { EventData } from "tns-core-modules/data/observable";
+import { Observable,EventData } from "tns-core-modules/data/observable";
 import { NavigatedData, Page } from "tns-core-modules/ui/page";
+import { Label } from "tns-core-modules/ui/label";
+import { StackLayout } from "tns-core-modules/ui/layouts/stack-layout";
+import { TabView, TabViewItem, SelectedIndexChangedEventData } from "tns-core-modules/ui/tab-view";
 
 import { HomeViewModel } from "./home-view-model";
 
@@ -14,3 +18,29 @@ export function onDrawerButtonTap(args: EventData) {
     const sideDrawer = <RadSideDrawer>app.getRootView();
     sideDrawer.showDrawer();
 }
+const stackLayout0 = new StackLayout();
+const label0 = new Label();
+label0.text = "Tab 0";
+stackLayout0.addChild(label0);
+
+const stackLayout1 = new StackLayout();
+const label1 = new Label();
+label1.text = "Tab 1";
+stackLayout1.addChild(label1);
+
+const tabViewItem0 = new TabViewItem();
+tabViewItem0.title = "Tab 0";
+tabViewItem0.view = stackLayout0;
+
+const tabViewItem1 = new TabViewItem();
+tabViewItem1.title = "Tab 1";
+tabViewItem1.view = stackLayout1;
+
+// creating TabView
+const tabView = new TabView();
+// setting up its items and the selected index
+const items = [];
+items.push(tabViewItem0);
+items.push(tabViewItem1);
+tabView.items = items;
+
