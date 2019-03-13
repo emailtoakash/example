348e826ba7b56543ba7c96e944b326338d4b62e3
Akash Singhal <akashsinghal@AkashSinghal.local>
2019-03-13 11:36:02 +0200

    first commit

---
+++ app/login/login-page.ts
@@ -0,0 +1,19 @@
+import { Observable, fromObject, EventData } from "tns-core-modules/data/observable";
+import { Button } from "tns-core-modules/ui/button";
+import { Page } from "tns-core-modules/ui/page";
+
+const user: Observable = fromObject({
+    email: 'akash@assetti.com',
+    password: 'password'
+});
+
+export function loaded(args: EventData): void {
+    const page: Page = <Page>args.object;
+    page.bindingContext = user;
+}
+
+export function onTap(args: EventData): void {
+    const button: Button = <Button>args.object;
+    const page: Page = button.page;
+    page.frame.navigate("home/home-page");
+}
