c46db7a6896ee7cf2453a9c86446c6ee8c1ead67
Elias Vähäjylkkä <vahajylkkaelias@gmail.com>
2019-03-14 17:54:15 +0200

    basics for new note

--- app/newnote/newnote-page.ts
+++ app/newnote/newnote-page.ts
@@ -1,9 +1,18 @@
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
-import { EventData } from "tns-core-modules/data/observable";
+import { Observable, EventData } from "tns-core-modules/data/observable";
 import { NavigatedData, Page } from "tns-core-modules/ui/page";
+import { TabView } from "tns-core-modules/ui/tab-view";
 
 import { NewNoteViewModel } from "./newnote-view-model";
+
+export function onLoaded(args) {
+    const tabView: TabView = <TabView>args.object;
+    const vm = new Observable();
+    vm.set("tabSelectedIndex", 0);
+
+    tabView.bindingContext = vm;
+}
 
 export function onNavigatingTo(args: NavigatedData) {
     const page = <Page>args.object;
@@ -13,4 +22,18 @@ export function onNavigatingTo(args: NavigatedData) {
 export function onDrawerButtonTap(args: EventData) {
     const sideDrawer = <RadSideDrawer>app.getRootView();
     sideDrawer.showDrawer();
+}
+
+export function changeTab(args) {
+    const vm = args.object.bindingContext;
+    const tabSelectedIndex = vm.get("tabSelectedIndex");
+    if (tabSelectedIndex === 0) {
+        vm.set("tabSelectedIndex", 1);
+    } else if (tabSelectedIndex === 1) {
+        vm.set("tabSelectedIndex", 2);
+    } else if (tabSelectedIndex === 2) {
+        vm.set("tabSelectedIndex", 3);
+    } else {
+        vm.set("tabSelectedIndex", 0);
+    }
 }
