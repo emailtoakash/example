b840c6bb8fcb7f5eb6140339956a1e37d594724d
Jonni <emailtoakash>
2019-03-14 11:57:34 +0200

    notes list in tab 1

--- app/home/home-page.ts
+++ app/home/home-page.ts
@@ -1,18 +1,19 @@
 
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
-import { Observable,EventData } from "tns-core-modules/data/observable";
+import { Observable,EventData,fromObject } from "tns-core-modules/data/observable";
 import { NavigatedData, Page } from "tns-core-modules/ui/page";
 import { Label } from "tns-core-modules/ui/label";
 import { StackLayout } from "tns-core-modules/ui/layouts/stack-layout";
+import { ListView, ItemEventData } from "tns-core-modules/ui/list-view";
 import { TabView, TabViewItem, SelectedIndexChangedEventData } from "tns-core-modules/ui/tab-view";
 
 import { HomeViewModel } from "./home-view-model";
-
+/*
 export function onNavigatingTo(args: NavigatedData) {
     const page = <Page>args.object;
     page.bindingContext = new HomeViewModel();
-}
+}*/
 
 export function onDrawerButtonTap(args: EventData) {
     const sideDrawer = <RadSideDrawer>app.getRootView();
@@ -43,4 +44,31 @@ const items = [];
 items.push(tabViewItem0);
 items.push(tabViewItem1);
 tabView.items = items;
+
+export function onNavigatingTo(args: EventData) {
+    const page = <Page>args.object;
+    const vm = fromObject({
+        myTitles: [
+            { title: 'First Title Here' },
+            { title: 'Second Title Here' },
+            { title: 'Third Title Here' },
+            { title: 'Fourth Title Here' },
+            { title: 'Fifth Title Here' },
+            { title: 'Sixth Title Here' },
+            { title: 'Seventh Title Here' },
+            { title: 'Title Number 8 Here' }
+        ]
+    });
+    page.bindingContext = vm;
+}
+
+export function onListViewLoaded(args: EventData) {
+    const listView = <ListView>args.object;
+}
+
+export function onItemTap(args: ItemEventData) {
+    const index = args.index;
+    console.log('ListView item tab ${index}');
+}
+
 
