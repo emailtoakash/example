14301741a7c09be7b25088a6e209ca9d03093d9f
Elias Vähäjylkkä <vahajylkkaelias@gmail.com>
2019-03-14 19:19:55 +0200

    it just works

--- app/newnote/newnote-page.ts
+++ app/newnote/newnote-page.ts
@@ -2,21 +2,14 @@ import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
 import { Observable, EventData } from "tns-core-modules/data/observable";
 import { NavigatedData, Page } from "tns-core-modules/ui/page";
-import { TabView } from "tns-core-modules/ui/tab-view";
-
 import { NewNoteViewModel } from "./newnote-view-model";
 
-export function onLoaded(args) {
-    const tabView: TabView = <TabView>args.object;
-    const vm = new Observable();
-    vm.set("tabSelectedIndex", 0);
-
-    tabView.bindingContext = vm;
-}
+let page, currentTab;
 
 export function onNavigatingTo(args: NavigatedData) {
-    const page = <Page>args.object;
+    page = <Page>args.object;
     page.bindingContext = new NewNoteViewModel();
+    currentTab = "a";
 }
 
 export function onDrawerButtonTap(args: EventData) {
@@ -25,15 +18,16 @@ export function onDrawerButtonTap(args: EventData) {
 }
 
 export function changeTab(args) {
-    const vm = args.object.bindingContext;
-    const tabSelectedIndex = vm.get("tabSelectedIndex");
-    if (tabSelectedIndex === 0) {
-        vm.set("tabSelectedIndex", 1);
-    } else if (tabSelectedIndex === 1) {
-        vm.set("tabSelectedIndex", 2);
-    } else if (tabSelectedIndex === 2) {
-        vm.set("tabSelectedIndex", 3);
+    let tab = page.getViewById(currentTab);
+    if (currentTab === "a") {
+        let nexttab = page.getViewById("b");
+        tab.className = "form hidden";
+        nexttab.className = "form";
+        currentTab = "b";
     } else {
-        vm.set("tabSelectedIndex", 0);
+        let nexttab = page.getViewById("a");
+        tab.className = "form hidden";
+        nexttab.className = "form";
+        currentTab = "a";
     }
 }
