e13078cf68d2896a381970d801e6ad6257156f9c
Elias Vähäjylkkä <vahajylkkaelias@gmail.com>
2019-03-14 14:32:51 +0200

    started on add note feature

--- app/newnote/newnote-page.ts
+++ app/newnote/newnote-page.ts
@@ -3,11 +3,11 @@ import * as app from "tns-core-modules/application";
 import { EventData } from "tns-core-modules/data/observable";
 import { NavigatedData, Page } from "tns-core-modules/ui/page";
 
-import { BrowseViewModel } from "./browse-view-model";
+import { NewNoteViewModel } from "./newnote-view-model";
 
 export function onNavigatingTo(args: NavigatedData) {
     const page = <Page>args.object;
-    page.bindingContext = new BrowseViewModel();
+    page.bindingContext = new NewNoteViewModel();
 }
 
 export function onDrawerButtonTap(args: EventData) {
--- app/newnote/newnote-view-model.ts
+++ app/newnote/newnote-view-model.ts
@@ -2,10 +2,10 @@ import { Observable } from "tns-core-modules/data/observable";
 
 import { SelectedPageService } from "../shared/selected-page-service";
 
-export class BrowseViewModel extends Observable {
+export class NewNoteViewModel extends Observable {
     constructor() {
         super();
 
-        SelectedPageService.getInstance().updateSelectedPage("Browse");
+        SelectedPageService.getInstance().updateSelectedPage("New Note");
     }
 }
