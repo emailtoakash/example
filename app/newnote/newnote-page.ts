fb461c85231f528df5dbfa88b15211dfb91809ff
emailtoakash <emailtoakash@gmail.com>
2019-03-16 11:38:32 +0200

    linking button image to new note view and fixing the login css

--- app/newnote/newnote-page.ts
+++ app/newnote/newnote-page.ts
@@ -3,6 +3,9 @@ import * as app from "tns-core-modules/application";
 import { Observable, EventData } from "tns-core-modules/data/observable";
 import { NavigatedData, Page } from "tns-core-modules/ui/page";
 import { NewNoteViewModel } from "./newnote-view-model";
+import { takePicture, requestPermissions } from 'nativescript-camera';
+import { android }from 'tns-core-modules/application';
+import { View } from 'tns-core-modules/ui/core/view';
 
 let page, currentTab;
 
@@ -30,4 +33,43 @@ export function changeTab(args) {
         nexttab.className = "form";
         currentTab = "a";
     }
+}
+export function onTakePictureTap(args: EventData) {
+    let page = <Page>(<View>args.object).page;
+    let saveToGallery = page.bindingContext.get("saveToGallery");
+    let keepAspectRatio = page.bindingContext.get("keepAspectRatio");
+    let width = page.bindingContext.get("width");
+    let height = page.bindingContext.get("height");
+    requestPermissions().then(
+        () => {
+            takePicture({ width: width, height: height, keepAspectRatio: keepAspectRatio, saveToGallery: saveToGallery }).
+                then((imageAsset) => {
+                    page.bindingContext.set("tns", imageAsset);
+                    imageAsset.getImageAsync(function (nativeImage) {
+                        let scale = 1;
+                        let actualWidth = 0;
+                        let actualHeight = 0;
+                        if (imageAsset.android) {
+                            // get the current density of the screen (dpi) and divide it by the default one to get the scale 
+                            //scale = nativeImage.getDensity() / android.util.DisplayMetrics.DENSITY_DEFAULT;
+                            scale = nativeImage.scale;
+                            actualWidth = nativeImage.getWidth();
+                            actualHeight = nativeImage.getHeight();
+                        } else {
+                            scale = nativeImage.scale;
+                            actualWidth = nativeImage.size.width * scale;
+                            actualHeight = nativeImage.size.height * scale;
+                        }
+
+
+                    
+
+                    });
+                },
+                    (err) => {
+                        console.log("Error -> " + err.message);
+                    });
+        },
+        () => alert('permissions rejected')
+    );
 }
