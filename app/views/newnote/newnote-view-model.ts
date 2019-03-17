7aa208718710c190aaf4fc90105a103feb453732
Elias Vähäjylkkä <vahajylkkaelias@gmail.com>
2019-03-17 17:09:34 +0200

    it works!

--- app/views/newnote/newnote-view-model.ts
+++ app/views/newnote/newnote-view-model.ts
@@ -1,10 +1,79 @@
 import { Observable } from "tns-core-modules/data/observable";
-
+import { ObservableArray } from "tns-core-modules/data/observable-array";
+import { TokenModel, RadAutoCompleteTextView } from "nativescript-ui-autocomplete";
 import { SelectedPageService } from "~/shared/selected-page-service";
+import { request } from 'tns-core-modules/http';
 
 export class NewNoteViewModel extends Observable {
-    constructor() {
+private autocomplete: RadAutoCompleteTextView;
+
+    constructor(page) {
         super();
+        this.autocomplete = <RadAutoCompleteTextView>page.getViewById("autocomplete");
+        this.autocomplete.loadSuggestionsAsync = function (text) {      
+            let properties;
+    
+            const promise = new Promise((resolve, reject) =>
+                request({
+                    url: "https://trial.assetti.pro/api/v2/properties?locale=EN&limit=50&offset=0",
+                    method: "GET",
+                    headers: { "Content-Type": "application/json" , "Authorization" : "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJBa2FzaC5TaW5naGFsQHN0dWRlbnQubHV0LmZpIiwiYXVkIjoiNEIxRDQ2OTAtQkQ5Qy00N0RGLUEzM0MtMTMzRUUyNzBEQTM5IiwiaWF0IjoxNTUyNzI3NzQyfQ.QRLfDQvvGT9wcWlelG4vPl5YcCaaJZbirBNzCZDYidQ"},
+                }).then((response) => {
+                    properties = response.content.toJSON();
+                    const dataItems: Array<TokenModel> = new Array();
+                    for (let i = 0; i < properties.length; i++) {
+                        dataItems.push(new TokenModel(properties[i].name, null));
+                    }
+                    resolve(dataItems);
+                }, (e) => {
+                    console.log("Error!")
+                    reject();
+                }
+            ));
+    
+            return promise;
+        }
+
         SelectedPageService.getInstance().updateSelectedPage("New Note");
+        // this.initDataItems();
+    }
+    
+    // private properties = ["Skinnarila", "Sammonlahti", "Leiri", "Keskusta"];
+
+    get dataItems(): ObservableArray<TokenModel> {
+        return this.get("_dataItems");
+    }
+
+    set dataItems(value: ObservableArray<TokenModel>) {
+        this.set("_dataItems", value);
+    }
+
+    private initDataItems(text) {
+        let dataItems = new ObservableArray<TokenModel>();
+
+        let properties;
+
+        const promise = new Promise((resolve, reject) =>
+            request({
+                url: "https://trial.assetti.pro/api/v2/properties?locale=EN&limit=5&offset=0",
+                method: "GET",
+                headers: { "Content-Type": "application/json" , "Authorization" : "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJBa2FzaC5TaW5naGFsQHN0dWRlbnQubHV0LmZpIiwiYXVkIjoiNEIxRDQ2OTAtQkQ5Qy00N0RGLUEzM0MtMTMzRUUyNzBEQTM5IiwiaWF0IjoxNTUyNzI3NzQyfQ.QRLfDQvvGT9wcWlelG4vPl5YcCaaJZbirBNzCZDYidQ"},
+            }).then((response) => {
+                properties = response.content.toJSON();
+                // console.log(response);
+                for (let i = 0; i < properties.length; i++) {
+                    dataItems.push(new TokenModel(properties[i].name, undefined));
+                }
+                console.log("DataItems during request:");
+                console.log(dataItems);
+                resolve(dataItems);
+            }, (e) => {
+                console.log("Error!")
+                reject();
+            }
+        ));
+
+        return promise;
     }
 }
+
