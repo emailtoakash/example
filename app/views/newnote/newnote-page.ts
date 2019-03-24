72b5ebb83e28c6a5fd68e20776bb76f989bdb6d1
emailtoakash <emailtoakash@gmail.com>
2019-03-25 00:01:38 +0200

    Linking camera feature to view note and some removal of old code

--- app/views/newnote/newnote-page.ts
+++ app/views/newnote/newnote-page.ts
@@ -84,46 +84,6 @@ export function changeTab(args) {
     }
 }
 
-    export function onTakePictureTap(args: EventData) {
-        let page = <Page>(<View>args.object).page;
-        let saveToGallery = page.bindingContext.get("saveToGallery");
-        let keepAspectRatio = page.bindingContext.get("keepAspectRatio");
-        let width = page.bindingContext.get("width");
-        let height = page.bindingContext.get("height");
-        requestPermissions().then(
-            () => {
-                takePicture({ width: width, height: height, keepAspectRatio: keepAspectRatio, saveToGallery: saveToGallery }).
-                    then((imageAsset) => {
-                        page.bindingContext.set("tns", imageAsset);
-                        imageAsset.getImageAsync(function (nativeImage) {
-                            let scale = 1;
-                            let actualWidth = 0;
-                            let actualHeight = 0;
-                            if (imageAsset.android) {
-                                // get the current density of the screen (dpi) and divide it by the default one to get the scale 
-                                //scale = nativeImage.getDensity() / android.util.DisplayMetrics.DENSITY_DEFAULT;
-                                scale = nativeImage.scale;
-                                actualWidth = nativeImage.getWidth();
-                                actualHeight = nativeImage.getHeight();
-                            } else {
-                                scale = nativeImage.scale;
-                                actualWidth = nativeImage.size.width * scale;
-                                actualHeight = nativeImage.size.height * scale;
-                            }
-
-                            
-                        
-
-                        });
-                    },
-                        (err) => {
-                            console.log("Error -> " + err.message);
-                        });
-            },
-            () => alert('permissions rejected')
-        );
-    }
-
 function backToHome() {
     topmost().navigate({
         moduleName: "views/notes-list/notes-list-page",
